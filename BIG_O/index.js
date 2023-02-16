e0fa925ba8e51b4a2a22b9064468b30872f25ce7
tanaliga100 <jordantanaliga@gmail.com>
2023-02-16 18:21:17 +0800

    big-o

--- BIG_O/index.js
+++ BIG_O/index.js
@@ -1,3 +1,4 @@
+// simmulation of Big-O || Time-Complexity
 function sumUp(a) {
   const res = (a / 2) * 1 + a;
   console.log(res);
@@ -15,3 +16,29 @@ function sumThis2(a) {
   }
 }
 sumThis2(10);
+
+// BETTER SOLUTION
+function sumNumbers(nums) {
+  let temp = 0; // once
+  for (const arg of nums) {
+    // once
+    console.log("..."); // seven times
+
+    temp = temp + arg; // seven times || cause it will loop the length of el in the array
+  }
+  return temp; // once
+}
+
+// NOT OPTIMIZED
+function sumNumbers2(nums) {
+  return nums.reduce((sum, currNum) => sum + currNum, 0);
+}
+
+let numbers = [1, 2, 5, 7, 8, 10, 100];
+const res = sumNumbers(numbers);
+const res2 = sumNumbers2(numbers);
+console.log(res2);
+
+// T =  1 + 1 + 1 + n + n = 3 + 2n = 3 + 2 * n
+// T = 2*n
+// T = n => 0(n) => Linear time complexity
