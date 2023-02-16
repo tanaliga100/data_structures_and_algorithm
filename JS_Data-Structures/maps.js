a4ed5e45f798379629f94def97867338134112a6
tanaliga100 <jordantanaliga@gmail.com>
2023-02-16 17:58:53 +0800

    maps

--- JS_Data-Structures/maps.js
+++ JS_Data-Structures/maps.js
@@ -2,3 +2,18 @@ console.log("MAPS");
 // ordered collections of key-value pairs
 // to retrieve you can use the corresponding key
 // maps are iterables. they can be used with a for-loop
+// use a map constructor to create a new map
+
+const map = new Map([
+  ["a", 1],
+  ["b", 2],
+]);
+map.set("c", 3);
+console.log(map.has("a")); // to check
+map.delete("c"); // to delete
+console.log(map.size); // size of the map
+map.clear();
+for (const [key, value] of map) {
+  console.log(`${key}: ${value}`);
+}
+console.log(map);
