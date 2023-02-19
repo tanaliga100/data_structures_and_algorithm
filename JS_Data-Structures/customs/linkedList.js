0ac4c44df3d508870c0e17c4583daeaef13a9dd8
tanaliga100 <jordantanaliga@gmail.com>
2023-02-19 17:11:20 +0800

    simulation

--- JS_Data-Structures/customs/linkedList.js
+++ JS_Data-Structures/customs/linkedList.js
@@ -9,12 +9,14 @@ console.log("LINKED LIST");
 // = Search > find an element given its values
 // = A real world application of STACKS || QUEUES
 
+// this node will be added to the list
 class Node {
   constructor(value) {
     this.value = value;
     this.next = null;
   }
 }
+// WILL serve as the list
 class LinkedList {
   constructor() {
     this.head = null;
@@ -26,7 +28,38 @@ class LinkedList {
   getSize() {
     return this.size;
   }
+  prepend(value) {
+    const node = new Node(value);
+    if (this.isEmpty()) {
+      this.head = node; // add this new Node if the list is empty
+      // in this case TRUE || empty;
+    } else {
+      node.next = this.head; // it will swap the to the second node
+      this.head = node; // now the 'this.had' will now be on the second index
+    }
+    this.size++; // and then increment;
+  }
+  print() {
+    if (this.isEmpty()) {
+      console.log("THE LIST IS EMPTY... Please add one");
+    } else {
+      let curr = this.head;
+      let listValues = [];
+      while (curr) {
+        listValues += `${curr.value} `;
+        curr = curr.next;
+      }
+      console.log(listValues);
+    }
+  }
 }
 const newList = new LinkedList();
 console.log(`List is empty`, newList.isEmpty());
 console.log(`List size`, newList.getSize());
+newList.print();
+newList.prepend(10);
+newList.print();
+newList.prepend(20);
+newList.print();
+newList.prepend(30);
+newList.print();
