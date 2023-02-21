41324c2c6d06b3e08c528d04f13314d61b92b2f8
tanaliga100 <jordantanaliga@gmail.com>
2023-02-21 18:02:20 +0800

    code

--- code.js
+++ code.js
@@ -17,3 +17,23 @@ function divisibleBy(numbers, divisor) {
   return numbers.filter((n) => n % divisor === 0);
 }
 console.log(divisibleBy([2, 3, 9, 12, 10], 2));
+
+//Name on billboard || cannot use '*' operator;
+function billboard(name, price = 30) {
+  return [...name].reduce((a, c) => {
+    console.log("curr", c);
+    console.log("acc", a);
+    return a + price;
+  }, 0);
+}
+console.log(billboard("lara is a nice woman"));
+
+// Opposites Attract
+function lovefunc(flower1, flower2) {
+  if (flower1 % 2 === 0 && flower2 % 2 === 0) return false;
+  if (flower1 % 2 !== 0 && flower2 % 2 !== 0) return false;
+  return true;
+}
+console.log(lovefunc(1, 2));
+console.log(lovefunc(2, 2));
+console.log(lovefunc(2, 1));
